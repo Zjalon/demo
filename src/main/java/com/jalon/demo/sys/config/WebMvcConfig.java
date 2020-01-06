4a86220e4b7c6769bd9fd6f681d156908acd671b
张健龙 <1642422726@qq.com>
2020-01-06 17:03:30 +0800

    前台结构,基础js,css引入

--- src/main/java/com/jalon/demo/sys/config/WebMvcConfig.java
+++ src/main/java/com/jalon/demo/sys/config/WebMvcConfig.java
@@ -1,5 +1,6 @@
 package com.jalon.demo.sys.config;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
@@ -9,13 +10,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
-        registry.addResourceHandler("/**").addResourceLocations("classpath:/templates/");
+        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
     }
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addRedirectViewController("","/index");
+        registry.addRedirectViewController(StringUtils.EMPTY, "/index");
         registry.addViewController("/index").setViewName("/index");
         registry.addViewController("/home").setViewName("/home");
         registry.addViewController("/login").setViewName("/login");
